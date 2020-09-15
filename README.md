3bb3a09c5e579c61a90db4fe16ceb84cacd35170
AbhinandD <abhinandd.me@gmail.com>
2020-09-15 18:34:28 +0530

    Updating Rename

---
+++ README.md
@@ -0,0 +1,23 @@
+## Rename Bot 
+
+An Open Source Telegram Rename Bot
+
+---
+👺 Rename any Telegram Files with Permanent Thumbnail Support
+
+IF YOU DEPLOY YOU WILL BE USING MY TEXTS IN YOUR COMMAND HAHAHA
+
+### Installation
+
+
+### You can tap the Deploy To Heroku button below to deploy straight to Heroku!
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+For Queries and support, contact [prgofficial](https://telegram.dog/prgofficial)
+
+## Credits, and Thanks to Beloved Developers ;
+
+* [SpEcHlDe](https://telegram.dog/SpEcHlDe) 
+* [Dan Tès](https://telegram.dog/haskell) 
+* [Yoily](https://telegram.dog/YoilyL)
+* [Anand](https://telegram.dog/Anandpskerala)
---
+++ translation.py
@@ -0,0 +1,36 @@
+class Translation(object):
+    START_TEXT = """ 
+    Hey, I'm File Rename Bot,
+
+<b>Send me any Telegram file & reply to that file with <code>/rename</code> FileName.ext</b>
+
+   Created  by @BG_OFFICIAL
+   Join Our Group for More Bots.
+   @BOT_GRAM_OFFICIAL"""
+
+    RENAME_403_ERR = "Hey This is My Boss Files,Don't touch them."
+    ABS_TEXT = "Are You Getting Mad??"
+    UPGRADE_TEXT = "<b>Upgrade Unavailable at Movement Please Try Again Later</b>"
+    DOWNLOAD_START = "Trying to Download Your File...😓"
+    UPLOAD_START = "<b>Your Download Completed🥰🥰</b>" \
+                   "Support @BOT_GRAM_OFFICIAL"
+    RCHD_TG_API_LIMIT = "Your File Downloaded {} seconds.\nDetected File Size: {}\nYou are Killing My Server!!!🥺"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > Support @BOT_GRAM_OFFICIAL **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Got Your Files in {} seconds.\nSent to You in {} seconds"
+    NOT_AUTH_USER_TEXT = "PM @BG_OFFICIAL"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Got This Msg PM @BG_OFFICIAL"
+    SAVED_CUSTOM_THUMB_NAIL = "<b>THUMBNAIL SAVED!!</b>🥰"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "🌬️<b>THUMNAIL CLEARED!!</b>💨"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media Cleared"
+    SAVED_RECVD_DOC_FILE = "Just Got Your File, Uploading Soon...😉"
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Thumbnail Saved😓"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    HELP_USER = """
+<b>It's Simple Don't Complicate it.</b>
+    Sent Your File Here to this bot
+    Sent Thumbnail for the file sent
+    Enter your new file name in this format
+     i.e, /rename New FileName.ext
+<b>Files Supported MP4,MKV,MP3,MOV....</b>"""
+

0e1617ce3ac9f4a27101beaaab15417a9bcb2ed6
tnwaps <65656829+tnwaps@users.noreply.github.com>
2020-10-11 21:18:22 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -4,21 +4,21 @@ class Translation(object):
 
 <b>Send me any Telegram file & reply to that file with <code>/rename</code> FileName.ext</b>
 
-   Created  by @BG_OFFICIAL
+   Created  by @tnmaza_support
    Join Our Group for More Bots.
-   @BOT_GRAM_OFFICIAL"""
+   @tnmaza_support """
 
     RENAME_403_ERR = "Hey This is My Boss Files,Don't touch them."
     ABS_TEXT = "Are You Getting Mad??"
     UPGRADE_TEXT = "<b>Upgrade Unavailable at Movement Please Try Again Later</b>"
     DOWNLOAD_START = "Trying to Download Your File...😓"
     UPLOAD_START = "<b>Your Download Completed🥰🥰</b>" \
-                   "Support @BOT_GRAM_OFFICIAL"
+                   "Support @tnmaza_support"
     RCHD_TG_API_LIMIT = "Your File Downloaded {} seconds.\nDetected File Size: {}\nYou are Killing My Server!!!🥺"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > Support @BOT_GRAM_OFFICIAL **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > Support @tnmaza_support **"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Got Your Files in {} seconds.\nSent to You in {} seconds"
-    NOT_AUTH_USER_TEXT = "PM @BG_OFFICIAL"
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "Got This Msg PM @BG_OFFICIAL"
+    NOT_AUTH_USER_TEXT = "PM @tnvip_bot"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Got This Msg PM @tnvip_bot"
     SAVED_CUSTOM_THUMB_NAIL = "<b>THUMBNAIL SAVED!!</b>🥰"
     DEL_ETED_CUSTOM_THUMB_NAIL = "🌬️<b>THUMNAIL CLEARED!!</b>💨"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media Cleared"
@@ -32,5 +32,5 @@ class Translation(object):
     Sent Thumbnail for the file sent
     Enter your new file name in this format
      i.e, /rename New FileName.ext
-<b>Files Supported MP4,MKV,MP3,MOV....</b>"""
+<b>Files Supported MP4,MKV,MP3,MOV.... @tnmaza_support </b>"""
 
